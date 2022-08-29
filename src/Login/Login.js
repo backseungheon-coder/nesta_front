c2e99b124b8f8d2c8de276e1bee46283d3eecddf
백승헌 <bsh7829@gmail.com>
2022-08-29 11:18:45 +0900

    https

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -31,7 +31,7 @@ function Login(props) {
     const onClickLogin = async () => { 
 
           
-            await fetch('http://api.nestatest.shop/rest-auth/login/', {
+            await fetch('https://api.nestatest.shop/rest-auth/login/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
